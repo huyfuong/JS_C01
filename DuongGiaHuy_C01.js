765a651dd2f5aca7804c12a9c1c692ca8448f3b0
Duonggiahuy <huyduong2812@gamil.com>
2022-05-07 13:30:35 +0700

    add c01

---
+++ DuongGiaHuy_C01.js
@@ -0,0 +1,64 @@
+//Bài 1
+console.log('Bai_1')
+console.log("\"I love car\"")
+
+//Bài 2 
+console.log('\nBai_2')
+let a = 4;
+let b = 5;
+let c = -5;
+let d = 2;
+let e = 0;
+let f = 3;
+let kq_2 = a / b;
+console.log('a/b =',kq_2)
+
+//Bài 3
+console.log('\nBai_3')
+let kg_3 = c + d;
+console.log(kg_3)
+
+//Bài 4 
+console.log('\nBai_4')
+let do_F = 37;
+let do_C = (5/9) * (do_F - 32);
+console.log(do_C)
+
+//Bài 5
+console.log('\nBai_5')
+let kq_5 = (b**2) + (c**2);
+console.log(kq_5)
+
+//Bài 6 
+console.log('\nBai_6')
+let kq_6 = Math.pow(a + b, 2);
+console.log(kq_6)
+
+//Bài 7
+console.log('\nBai_7')
+let kq_7 = ((a*a)*(b*b)) / d;
+console.log(kq_7)
+
+//Bài 8
+console.log('\nBai_8')
+let toan = 8;
+let ly = 6;
+let sinh = 7;
+let anh = 8;
+let kq_8 = (toan + ly + sinh + anh) / 4;
+console.log(kq_8)
+
+//Bài 9
+console.log('\nBai_9')
+let kq_9 = f/e;
+console.log(kq_9)
+
+//Bài 10 
+console.log('\nBai_10')
+let kq_10 = (-b + Math.sqrt(b*b - 4 * f * c)) / (2*f);
+console.log(kq_10)
+
+//Bài 11
+console.log('\nBai_11')
+let s = (Math.sqrt(a) + Math.abs(a)) / (Math.sqrt(Math.pow(a,d)));
+console.log(s)
